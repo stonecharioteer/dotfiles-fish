beea3a32981fcc05ada58cd0d96fd881a49ea470
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-12-08 14:28:23 +0530

    feat: add abbrs

--- config.fish
+++ config.fish
@@ -6,12 +6,15 @@ if status is-interactive
     zoxide init fish | source
     fish_vi_key_bindings
     fx --comp fish | source
+    direnv hook fish | source
     source $HOME/.local/bin/env.fish
+    # ~/.rbenv/bin/rbenv init - --no-rehash fish | source
 end
 
 # Aliases
 alias vim='nvim'
 # alias rm='echo \'Don\'t do zat.\''
-
 fish_add_path $HOME/code/checkouts/personal/scripts
 fish_add_path $HOME/.cargo/bin
+
+# Added by `rbenv init` on Mon Sep 22 02:27:36 PM IST 2025
