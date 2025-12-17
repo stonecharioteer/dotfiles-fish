5f713c86acaabc56713faa59d07720707e87794e
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-12-17 18:57:32 +0530

    feat: add ruby abbrs, move mise source out of interactive so that it's sourced on all envs

--- config.fish
+++ config.fish
@@ -1,6 +1,11 @@
+$HOME/.local/bin/mise activate fish | source
+# Aliases
+alias vim='nvim'
+# alias rm='echo \'Don\'t do zat.\''
+fish_add_path $HOME/code/checkouts/personal/scripts
+fish_add_path $HOME/.cargo/bin
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    $HOME/.local/bin/mise activate fish | source
     starship init fish | source
     atuin init fish | source
     zoxide init fish | source
@@ -8,13 +13,4 @@ if status is-interactive
     fx --comp fish | source
     direnv hook fish | source
     source $HOME/.local/bin/env.fish
-    # ~/.rbenv/bin/rbenv init - --no-rehash fish | source
 end
-
-# Aliases
-alias vim='nvim'
-# alias rm='echo \'Don\'t do zat.\''
-fish_add_path $HOME/code/checkouts/personal/scripts
-fish_add_path $HOME/.cargo/bin
-
-# Added by `rbenv init` on Mon Sep 22 02:27:36 PM IST 2025
